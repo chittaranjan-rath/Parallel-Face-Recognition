df79ecba114df06bd66c38e79c00cb73313e6402
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2019-04-04 02:39:43 +0530

    update

--- src/data_preprocessing/csv_to_vector.cpp
+++ src/data_preprocessing/csv_to_vector.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h> 
 #include <boost/algorithm/string.hpp>
+#include <omp.h>
+#define THREADS 4
 
 using namespace std;
+
 
 typedef unsigned char uchar;
 
@@ -93,7 +96,7 @@ vector<int> average_face(vector<vector<int>>& image_pixel,vector<vector<string>>
   return avg_face;
 }
 
-vector<vector<int>> generate_all_avg_face(vector<vector<int>>& image_pixel,vector<vector<string>>& img_info){
+vector<vector<int>> generate_all_avg_face(vector<vector<int>>& image_pixel,vector<vector<string>>& img_info,vector<string>& avg_face_info){
 
   vector<vector<int>> avg_face;
   string filename = "";
@@ -104,6 +107,8 @@ vector<vector<int>> generate_all_avg_face(vector<vector<int>>& image_pixel,vecto
 
     if(subject_id != img_info[i][0]){
 
+        avg_face_info.push_back(img_info[i][0]);
+
         filename = img_info[i][0];
         print_image(image_pixel[i],filename);
         
@@ -111,7 +116,7 @@ vector<vector<int>> generate_all_avg_face(vector<vector<int>>& image_pixel,vecto
         avg_face.push_back(average_face(image_pixel,img_info,img_info[i][0]));
         
         filename = img_info[i][0]+"avg";
-        print_image(avg_face[count],filename);
+        // print_image(avg_face[count],filename);
 
         count++;
 
@@ -120,7 +125,7 @@ vector<vector<int>> generate_all_avg_face(vector<vector<int>>& image_pixel,vecto
   return avg_face;
 }
 
-void read_from_csv(vector<vector<int>>& image,vector<vector<int>>& img_info,string filename){
+void read_from_csv(vector<vector<int>>& image,vector<vector<string>>& img_info,string filename){
 
   string line;
   ifstream image_dataset (filename);
@@ -142,34 +147,126 @@ void read_from_csv(vector<vector<int>>& image,vector<vector<int>>& img_info,stri
 double euclidean_distance(vector<int>image1,vector<int>image2){
 
   double dist = 0.0;
+  // #pragma omp parallel for schedule(static) num_threads(THREADS) 
+  for(int i = 0;i<image1.size();i++){
 
-  
+    // #pragma omp atomic
+    dist += pow((image1[i] - image2[i]),2);
+
+  }
+
+  dist = sqrt(dist);
 
   return dist;
 }
 
+void predict(vector<vector<int>>& avg_face,vector<vector<int>>& test_images,vector<string>& avg_face_info,vector<string>& predicted_image_info){
+
+  double min_distance = 0.0,temp;
+
+  for(int i = 0;i<test_images.size();i++){
+  
+    min_distance = 0.0;
+
+    // #pragma omp parallel for schedule(static) num_threads(THREADS)
+    for(int j = 0;j<avg_face.size();j++){
+
+      if(j==0){
+        min_distance = euclidean_distance(avg_face[j],test_images[i]);
+        predicted_image_info[i] = avg_face_info[j];
+      }else{
+
+        temp = euclidean_distance(avg_face[j],test_images[i]);
+
+        if(temp<min_distance){
+
+          min_distance = temp;
+          predicted_image_info[i] = avg_face_info[j];
+        }
+
+      }
+
+    }
+
+  }
+    
+}
+
+double calculate_accuracy(vector<string>& predicted_image_info,vector<vector<string>>& test_image_info){
+
+  double total_size = predicted_image_info.size();
+  double correct_prediction = 0.0;
+  double accuracy = 0.0;
+
+  for(int i = 0;i<predicted_image_info.size();i++){
+
+    if(predicted_image_info[i] == test_image_info[i][0]){
+      correct_prediction += 1.0;
+    }
+
+  }
+
+  accuracy = correct_prediction/total_size;
+
+  return accuracy;
+}
+
+void visulalize_output(vector<vector<int>>& test_images,vector<string>& predicted,vector<vector<string>>& actual){
+
+  string filename = "";
+  for(int i = 0;i<actual.size();i++){
+    filename = actual[i][0]+" vs "+predicted[i];
+    print_image(test_images[i],filename);
+  }
+
+}
+
 int main () {
+
+
+double t = omp_get_wtime();
+
+// #pragma omp parallel for schedule(dynamic) num_threads(THREADS)
+// #pragma omp parallel for schedule(static) num_threads(THREADS)
+// #pragma omp parallel for schedule(guided) num_threads(THREADS)
+// #pragma omp parallel for schedule(dynamic,100) num_threads(THREADS)
 
   
   vector<vector<int>> avg_face;
   vector<vector<int>> train_images;
+  vector<string> avg_face_info;
   vector<vector<string>> train_image_info;
 
   vector<vector<int>> test_images;
   vector<vector<string>> test_image_info;
-  vector<vector<string>> predicted_image_info;
-  
+  vector<string> predicted_image_info;
+
+  double accuracy = 0.0;
+
   string filename = "train_image_dataset.csv";
 
   read_from_csv(train_images,train_image_info,filename);  
 
-  avg_face = generate_all_avg_face(train_images,train_image_info);
+  avg_face = generate_all_avg_face(train_images,train_image_info,avg_face_info);
 
   filename = "test_image_dataset.csv";
 
   read_from_csv(test_images,test_image_info,filename);  
+  
+  predicted_image_info.resize(test_images.size());
 
-  predict(avg_face,test_images,train_image_info,predicted_image_info);
+  predict(avg_face,test_images,avg_face_info,predicted_image_info);
+
+
+  accuracy = calculate_accuracy(predicted_image_info,test_image_info);
+
+  cout<<"Accuracy: "<<accuracy<<"\n";
+
+  visulalize_output(test_images,predicted_image_info,test_image_info);
+
+  double f = omp_get_wtime();
+
+  printf("%f\n",f-t);
 
   return 0;
 
