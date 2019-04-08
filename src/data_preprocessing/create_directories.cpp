605f9e829a14313d383723fd064bacd7c5db97f6
chitta <chitta.iiith@gmail.com>
2019-04-08 13:27:31 +0530

    create required directories

---
+++ src/data_preprocessing/create_directories.cpp
@@ -0,0 +1,27 @@
+#include <sys/stat.h>
+#include<stdio.h>
+using namespace std;
+int main(){
+    int dir_err = mkdir("avg_face", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (-1 == dir_err)
+    {
+        printf("Error creating directory avg_face !n");
+    }
+    dir_err = mkdir("avg_face_parallel", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (-1 == dir_err)
+    {
+        printf("Error creating directory avg_face_parallel !n");
+    }
+    dir_err = mkdir("prediction_knn", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (-1 == dir_err)
+    {
+        printf("Error creating directory prediction_knn !n");
+    }
+    dir_err = mkdir("prediction_mean", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (-1 == dir_err)
+    {
+        printf("Error creating directory prediction_mean !n");
+    }
+
+
+}
